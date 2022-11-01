03c8cc6ebe4757f3cf894b02d1eb74d15238063d
B0268630 <isha@airtel.com>
2022-11-01 19:13:17 +0530

    controller changes

--- src/main/java/com/example/multirole/service/UserDetailsService.java
+++ src/main/java/com/example/multirole/service/UserDetailsService.java
@@ -1,7 +1,7 @@
-package com.example.multirole.service;
-
-import com.example.multirole.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDetailsService extends JpaRepository<User, Long> {
-}
+//package com.example.multirole.service;
+//
+//import com.example.multirole.entity.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface UserDetailsService extends JpaRepository<User, Long> {
+//}
